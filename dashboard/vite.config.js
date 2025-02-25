baf5238260df5108dffa0dab39d081096ac61a39
Yogesh Pote <yogpote035@gmail.com>
2025-02-25 14:54:24 +0530

    upadte vite config

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, 
-    host: '0.0.0.0',             
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
   },
   preview: {
-    allowedHosts: 'all',            
+    allowedHosts: ['zerodha-kite-yogesh.onrender.com', 'all'],
   }
 })
